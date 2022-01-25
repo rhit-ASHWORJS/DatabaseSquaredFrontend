47eff93c363da7acb4b2415d6af4e298574632eb
rhit-SlaterE <slatere@rose-hulman.edu>
2022-01-25 17:37:20 -0500

    added CRUD for comapany and Database

--- src/CRUD/CompanyCRUD.java
+++ src/CRUD/CompanyCRUD.java
@@ -1,5 +1,9 @@
 package CRUD;
 
+import java.sql.CallableStatement;
+import java.sql.SQLException;
+import java.sql.Types;
+
 import databasesquared.services.DatabaseConnectionService;
 
 public class CompanyCRUD {
@@ -9,7 +13,26 @@ public class CompanyCRUD {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public boolean updateCompany() {
+	public boolean updateCompany(int id, String name, int employeeCount) {
+		try {
+			CallableStatement cs = dbService.getConnection().prepareCall("? = call updateCompany(?,?,?)");
+			cs.registerOutParameter(1, Types.INTEGER);
+			cs.setInt(2, id);
+			cs.setString(3, name);
+			cs.setInt(4, employeeCount);
+			cs.execute();
+			int returnValue = cs.getInt(1);
+			switch (returnValue) {
+			case 1:
+				System.out.println("updateCompany error 1");
+				break;
+			default:
+				return true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();	
+		}
 		return false;
 	}
 }
--- src/CRUD/DatabaseCRUD.java
+++ src/CRUD/DatabaseCRUD.java
@@ -1,5 +1,10 @@
 package CRUD;
 
+import java.sql.CallableStatement;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
 import java.util.ArrayList;
 
 import databasesquared.services.DatabaseConnectionService;
@@ -11,17 +16,114 @@ public class DatabaseCRUD {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public boolean addDatabase() {
+	public boolean addDatabase(String name, String DBMSName, String description, Date date, int tableCount) {
+		try {
+			CallableStatement cs = dbService.getConnection().prepareCall("? = call addDatabase(?,?,?,?,?)");
+			cs.registerOutParameter(1, Types.INTEGER);
+			cs.setString(2, name);
+			cs.setString(3, DBMSName);
+			cs.setString(4, description);
+			cs.setDate(5, date);
+			cs.setInt(6, tableCount);
+			cs.execute();
+			int returnValue = cs.getInt(1);
+			switch (returnValue) {
+			case 1:
+				System.out.println("addDatabase error 1");
+				break;
+			default:
+				return true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();	
+		}
 		return false;
 	}
 	
-	public boolean updateDatabase() {
+	public boolean updateDatabase(int id, String name, String DBMSName, String description, Date date, int tableCount) {
+		try {
+			CallableStatement cs = dbService.getConnection().prepareCall("? = call updateDatabase(?,?,?,?,?,?)");
+			cs.registerOutParameter(1, Types.INTEGER);
+			cs.setInt(2, id);
+			cs.setString(3, name);
+			cs.setString(4, DBMSName);
+			cs.setString(5, description);
+			cs.setDate(6, date);
+			cs.setInt(7, tableCount);
+			cs.execute();
+			int returnValue = cs.getInt(1);
+			switch (returnValue) {
+			case 1:
+				System.out.println("updateDatabase error 1");
+				break;
+			default:
+				return true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();	
+		}
 		return false;
 	}
-	public boolean deleteDatabase() {
+	public boolean deleteDatabase(int id) {
+		try {
+			CallableStatement cs = dbService.getConnection().prepareCall("? = call deleteDatabase(?)");
+			cs.registerOutParameter(1, Types.INTEGER);
+			cs.setInt(2, id);
+			cs.execute();
+			int returnValue = cs.getInt(1);
+			switch (returnValue) {
+			case 1:
+				System.out.println("deleteDatabase error 1");
+				break;
+			default:
+				return true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();	
+		}
 		return false;
 	}
-	public ArrayList<String> getDatabase(){
+	public ArrayList<String> getDatabase(int id){
+		try {
+			CallableStatement cs = dbService.getConnection().prepareCall("? = call getDatabase(?)");
+			cs.registerOutParameter(1, Types.INTEGER);
+			cs.setInt(2, id);
+			ResultSet rs = cs.executeQuery();
+			int returnValue = cs.getInt(1);
+			switch (returnValue) {
+			case 1:
+				System.out.println("deleteDatabase error 1");
+				break;
+			default:
+				return convertSet(rs);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();	
+		}
 		return null;
+	}
+
+	private ArrayList<String> convertSet(ResultSet rs) {
+		ArrayList<String> list = new ArrayList<>();
+		try {
+			while(rs.next()) {
+				StringBuilder sb = new StringBuilder();
+				sb.append(rs.getString(1));
+				sb.append(rs.getString(2));
+				sb.append(rs.getString(3));
+				sb.append(rs.getDate(4));
+				sb.append(rs.getInt(5));
+				sb.append("\n");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// TODO Auto-generated method stub
+		return list;
 	}
 }
