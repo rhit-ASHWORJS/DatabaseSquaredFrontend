b5741abaed1be71c520295c82c4d78cfa0172071
rhit-SlaterE <slatere@rose-hulman.edu>
2022-02-01 16:03:57 -0500

    Fixed filter problem

--- src/CRUD/FilterCRUD.java
+++ src/CRUD/FilterCRUD.java
@@ -15,13 +15,14 @@ public class FilterCRUD {
 		this.dbService = dbService;
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	/**
-	 * filters the reviews 
+	 * filters the reviews
+	 * 
 	 * @param reviewer nullable
-	 * @param DBMS nullable
-	 * @param company nullable
-	 * @param score nullable -1 is null
+	 * @param DBMS     nullable
+	 * @param company  nullable
+	 * @param score    nullable -1 is null
 	 * @return rs
 	 */
 	public ResultSet filterReviews(String reviewer, String DBMS, String company, int score) {
@@ -29,13 +30,13 @@ public class FilterCRUD {
 			CallableStatement cs = dbService.getConnection().prepareCall("{? = call filterReviews(?,?,?,?)}");
 			cs.registerOutParameter(1, Types.INTEGER);
 			cs.setString(2, reviewer);
+
 			cs.setString(3, DBMS);
+
 			cs.setString(4, company);
-			if(score == -1) {
-				cs.setNull(5, Types.NULL);
-			}else {
-				cs.setInt(5, score);
-			}
+
+			cs.setInt(5, score);
+
 			cs.execute();
 			int returnValue = cs.getInt(1);
 			ResultSet rs = cs.executeQuery();
@@ -48,27 +49,27 @@ public class FilterCRUD {
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();	
+			e.printStackTrace();
 		}
 		return null;
 	}
-	
 
 	/**
 	 * 
 	 * @param rs
 	 * @return ReviewerName, DBMS Company, score, text
 	 */
-	public ArrayList<ArrayList<String>> parceFilteredReviews(ResultSet rs){
+	public ArrayList<ArrayList<String>> parceFilteredReviews(ResultSet rs) {
 		ArrayList<ArrayList<String>> list = new ArrayList<>();
 		int index = 0;
 		try {
-			while(rs.next()) {
+			System.out.println(rs.toString());
+			while (rs.next()) {
 				list.add(new ArrayList<>());
 				list.get(index).add(rs.getString(1));
 				list.get(index).add(rs.getString(2));
 				list.get(index).add(rs.getString(3));
-				list.get(index).add(rs.getInt(4)+"");
+				list.get(index).add(rs.getInt(4) + "");
 				list.get(index).add(rs.getString(5));
 				index++;
 			}
@@ -78,11 +79,11 @@ public class FilterCRUD {
 		}
 		return list;
 	}
-	
+
 	/**
 	 * 
 	 * @param company nullable
-	 * @param score nullable -1 is null 
+	 * @param score   nullable -1 is null
 	 * @return
 	 */
 	public ResultSet filterDBMS(String company, int score) {
@@ -90,11 +91,7 @@ public class FilterCRUD {
 			CallableStatement cs = dbService.getConnection().prepareCall("{? = call filterDBMS(?,?)}");
 			cs.registerOutParameter(1, Types.INTEGER);
 			cs.setString(2, company);
-			if(score == -1) {
-				cs.setNull(3, Types.NULL);
-			}else {
-				cs.setInt(3, score);
-			}
+			cs.setInt(3, score);
 			cs.execute();
 			int returnValue = cs.getInt(1);
 			ResultSet rs = cs.executeQuery();
@@ -107,26 +104,26 @@ public class FilterCRUD {
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();	
+			e.printStackTrace();
 		}
 		return null;
 	}
-	
+
 	/**
 	 * 
 	 * @param rs
-	 * @return DBMS, Company, NumFatabases, AvgScore	
+	 * @return DBMS, Company, NumFatabases, AvgScore
 	 */
-	public ArrayList<ArrayList<String>> parceFilteredDBMS(ResultSet rs){
+	public ArrayList<ArrayList<String>> parceFilteredDBMS(ResultSet rs) {
 		ArrayList<ArrayList<String>> list = new ArrayList<>();
 		int index = 0;
 		try {
-			while(rs.next()) {
+			while (rs.next()) {
 				list.add(new ArrayList<>());
 				list.get(index).add(rs.getString(1));
 				list.get(index).add(rs.getString(2));
-				list.get(index).add(rs.getInt(3)+"");
-				list.get(index).add(rs.getDouble(4)+"");
+				list.get(index).add(rs.getInt(3) + "");
+				list.get(index).add(rs.getDouble(4) + "");
 				index++;
 			}
 		} catch (SQLException e) {
@@ -135,16 +132,16 @@ public class FilterCRUD {
 		}
 		return list;
 	}
-	
+
 	/**
 	 * 
 	 * @param usedDBMS nullable
 	 * @param madeDBMS nullable
 	 * @return
 	 */
-	public ResultSet filterCompanies(String usedDBMS,String madeDBMS) {
+	public ResultSet filterCompanies(String usedDBMS, String madeDBMS) {
 		try {
-			CallableStatement cs = dbService.getConnection().prepareCall("{? = call getListedReviews(?,?)}");
+			CallableStatement cs = dbService.getConnection().prepareCall("{? = call filterCompanies(?,?)}");
 			cs.registerOutParameter(1, Types.INTEGER);
 			cs.setString(2, usedDBMS);
 			cs.setString(3, madeDBMS);
@@ -153,31 +150,31 @@ public class FilterCRUD {
 			ResultSet rs = cs.executeQuery();
 			switch (returnValue) {
 			case 1:
-				System.out.println("getListedReviews error 1");
+				System.out.println("filterCompanies error 1");
 				break;
 			default:
 				return rs;
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();	
+			e.printStackTrace();
 		}
 		return null;
 	}
-	
+
 	/**
 	 * 
 	 * @param rs
 	 * @return Company, NumEmployees, UsedDBMS, MadeDBMS
 	 */
-	public ArrayList<ArrayList<String>> parceFilteredCompanies(ResultSet rs){
+	public ArrayList<ArrayList<String>> parceFilteredCompanies(ResultSet rs) {
 		ArrayList<ArrayList<String>> list = new ArrayList<>();
 		int index = 0;
 		try {
-			while(rs.next()) {
+			while (rs.next()) {
 				list.add(new ArrayList<>());
 				list.get(index).add(rs.getString(1));
-				list.get(index).add(rs.getInt(2)+"");
+				list.get(index).add(rs.getInt(2) + "");
 				list.get(index).add(rs.getString(3));
 				list.get(index).add(rs.getString(4));
 				index++;
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,3 @@
-import java.sql.Date;
-import java.time.LocalDate;
-import java.util.Calendar;
-
 import CRUD.FullCRUD;
 import UI.UIMain;
 import databasesquared.services.DatabaseConnectionService;
@@ -10,16 +6,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Connecting...");
-//		ApplicationRunner appRunner = new ApplicationRunner();
-//		appRunner.runApplication(args);
 		DatabaseConnectionService test = new DatabaseConnectionService("titan.csse.rose-hulman.edu","DatabaseSquared");
 		test.connect("dbSquareUser", "ilikedatabases");
 		FullCRUD fc = new FullCRUD(test);
-//		System.out.println(fc.getReviewList("bob"));
-//		System.out.println(fc.getListedReviews());
-//		System.out.println(fc.getCompanyInfo(1));
 		System.out.println("Connected");
-//		
 		UIMain ui = new UIMain(fc);
 		ui.beginUI();
 	}
